6944587f1b0bc3196451d970864460c62929ace2
luAr26101 <raul.cosmin.26@icloud.com>
2026-04-18 15:44:04 +0300

    Update README.md and add the assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="random-class text-3xl font-black">Comfy store homepage</h1>
+      <h1 className="text-3xl font-black">Comfy store</h1>
     </>
   );
 }
